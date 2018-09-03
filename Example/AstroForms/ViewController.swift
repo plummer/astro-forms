742059875b68b0ecef28198554fb56cb089b411b
Andrew Plummer <andrewplummer@me.com>
2018-09-04 08:56:36 +1000

    [FEAT] Implement default pod structure
    A few small changes to remove warnings included.

---
+++ Example/AstroForms/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AstroForms
+//
+//  Created by plummer on 09/04/2018.
+//  Copyright (c) 2018 plummer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
