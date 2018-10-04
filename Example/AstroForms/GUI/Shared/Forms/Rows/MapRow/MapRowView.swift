0fab281cc55c04fdeb7f5dc586209d30ba912733
Andrew Plummer <andrewplummer@me.com>
2018-10-04 10:39:37 +1000

    [FEAT] Initial map row stub

---
+++ Example/AstroForms/GUI/Shared/Forms/Rows/MapRow/MapRowView.swift
@@ -0,0 +1,16 @@
+//
+//  MapRowView.swift
+//  AstroForms_Example
+//
+//  Created by Andrew Plummer on 30/9/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapRowView: UIView {
+    
+    weak var row: MapRow?
+    
+}
